4b9bf1688314ad95d4a2dd74797911cce26e67a4
brad.wu <brad.wu@ebaotech.com>
2024-04-28 20:34:04 +0800

    initialize

--- src/assets/icons/module-source-files.tsx
+++ src/assets/icons/module-source-files.tsx
@@ -1,40 +1,16 @@
 import React, {SVGProps} from 'react';
 
 export const ModuleSourceFilesIcon = (props: Omit<SVGProps<SVGSVGElement>, 'xmlns' | 'viewBox'>) => {
-	return <svg {...props} data-icon="f1-module-source-files" viewBox="0 0 24 24" fill="none"
-	            xmlns="http://www.w3.org/2000/svg">
+	return <svg {...props} data-icon="f1-module-source-files" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+		<path d="M27.5,5.5H18.2L16.1,9.7H4.4V26.5H29.6V5.5Zm0,4.2H19.3l1.1-2.1h7.1Z" fill="#14622a"/>
 		<path
-			d="M9.6 20H14.4C18.4 20 20 18.4 20 14.4V9.6C20 5.6 18.4 4 14.4 4H9.6C5.6 4 4 5.6 4 9.6V14.4C4 18.4 5.6 20 9.6 20Z"
-			stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-		<path opacity="0.4"
-		      d="M10.5 17H13.5C16 17 17 16 17 13.5V10.5C17 8 16 7 13.5 7H10.5C8 7 7 8 7 10.5V13.5C7 16 8 17 10.5 17Z"
-		      stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-		<path opacity="0.4" d="M8.00977 4V2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M12 4V2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M16 4V2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M20 8H22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M20 12H22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M20 16H22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M16 20V22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M12.0098 20V22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M8.00977 20V22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M2 8H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M2 12H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
-		<path opacity="0.4" d="M2 16H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-		      strokeLinejoin="round"/>
+			d="M19.146,30.989a.9.9,0,0,1-.207-.025,1.041,1.041,0,0,1-.726-1.213L20.922,15.32a1.037,1.037,0,0,1,.444-.683.894.894,0,0,1,.7-.122,1.043,1.043,0,0,1,.727,1.213L20.077,30.16a1.033,1.033,0,0,1-.442.681A.9.9,0,0,1,19.146,30.989Z"
+			fill="#06cc14"/>
 		<path
-			d="M11.9997 9.7002L11.0597 11.3402C10.8497 11.7002 11.0197 12.0002 11.4397 12.0002H12.5597C12.9797 12.0002 13.1497 12.3002 12.9397 12.6602L11.9997 14.3002"
-			stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+			d="M24.578,28.944l-.068,0a.931.931,0,0,1-.668-.377,1.1,1.1,0,0,1,.1-1.419L28.6,22.595l-4.638-4.239a1.1,1.1,0,0,1-.141-1.416.937.937,0,0,1,.661-.4.9.9,0,0,1,.709.237l5.47,5a1.1,1.1,0,0,1,.144,1.416,1.061,1.061,0,0,1-.142.163L25.216,28.68A.916.916,0,0,1,24.578,28.944Z"
+			fill="#06cc14"/>
+		<path
+			d="M16.423,28.947a.914.914,0,0,1-.639-.267l-5.452-5.327A.9.9,0,0,1,10.2,23.2a1.094,1.094,0,0,1,.141-1.414l5.471-5a.881.881,0,0,1,.7-.238.941.941,0,0,1,.665.4,1.1,1.1,0,0,1-.14,1.417L12.4,22.6l4.659,4.551a1.093,1.093,0,0,1,.1,1.419.927.927,0,0,1-.669.377Z"
+			fill="#06cc14"/>
 	</svg>;
 };
